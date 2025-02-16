b259be73a66815f92c1c4c456bec16fb96fcfa44
Taylor <taylorkaake@gmail.com>
2025-02-15 22:19:19 -0500

    Restructured code, using commands.bot instead of discord.client now, auto rolling works, claims next

--- bot.py
+++ bot.py
@@ -1,44 +1,125 @@
+import sys, traceback, configparser
+import datetime as dt
 from time import sleep
-
-import discord
-import configparser
-
-from discord import SlashCommand
-from discord.ext.commands import command
+from scheduler.asyncio import Scheduler
+from discord.ext import commands
+from mudae import MudaeCommander
+import threading
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-token = config['UserInfo']['token']
+user_token = config['UserInfo']['token']
 channel_id = int(config['ServerInfo']['channel_ID'])
 
-client = discord.Client(request_guilds = False)
+prefix = "!"
 mudae_channel = None
-command_dict = {}
+mudae = None
+auto_roll_enabled = False
+bot = commands.Bot(command_prefix="?", description="Weeb shit", request_guilds = False, self_bot=True)
+cease_continuous_run = threading.Event()
+threadyboi = None
+schedule = None
 
-@client.event
+
+#define checks
+def is_me():
+    def predicate(ctx):
+        return ctx.message.author == bot.user
+    return commands.check(predicate)
+
+def is_mudae_channel():
+    def predicate(ctx):
+        return ctx.message.channel == mudae_channel
+    return commands.check(predicate)
+
+@bot.event
 async def on_ready():
-    print(f'Logged in as user {client.user}')
-    mudae_channel = client.get_channel(channel_id)
-    print(mudae_channel)
+    global mudae_channel
+    global mudae
+
+    # Retrieve Mudae Channel, create mudae commander, and load commands
+    mudae_channel = bot.get_channel(channel_id)
+    mudae = MudaeCommander({},mudae_channel)
     cmds = await mudae_channel.application_commands()
     for cmd in cmds:
         if cmd.id == 832171928072224789:
-            command_dict["Timer"] = cmd
-            print(cmd)
+            mudae.command_dict['tu'] = cmd
+        if cmd.id ==832172151729422417:
+            mudae.command_dict['wa'] = cmd
+        if cmd.id ==832172457028747336:
+            mudae.command_dict['ha'] = cmd
+        if cmd.id ==832172216665374750:
+            mudae.command_dict['wg'] = cmd
+        if cmd.id ==832172416192872458:
+            mudae.command_dict['hg'] = cmd
+        if cmd.id ==832172261968314388:
+            mudae.command_dict['wx'] = cmd
+            mudae.command_dict['w'] = cmd
+        if cmd.id ==832172373536669706:
+            mudae.command_dict['hx'] = cmd
+            mudae.command_dict['h'] = cmd
+    print(f'Logged in as user {bot.user}')
     sleep(1)
 
+@is_me()
+@is_mudae_channel()
+@bot.command(name='rollslam',
+             aliases=['rs'])
+async def roll_slam(ctx, category:str, rolls:int):
+    if category in ['wa', 'wg', 'w', 'ha', 'hg', 'h']:
+        if rolls > 0:
+            print('Parsing successful')
+            await mudae.send_rolls(category, rolls)
+        else:
+            await mudae_channel.send(f'Invalid number of rolls specified, must be > 0')
+            return
+    else:
+        await mudae_channel.send(f'Invalid category specified. Valid categories are: wa, wg, w, ha, hg, h')
+        return
+
+@is_me()
+@is_mudae_channel()
+@bot.command(name='disableeautoroll',
+             aliases=['dar'])
+async def disable_auto_roll(ctx):
+    global schedule
+    schedule = Scheduler()
+    await mudae_channel.send(f'Auto roll disabled')
+
+@is_me()
+@is_mudae_channel()
+@bot.command(name='enableautoroll',
+             aliases=['ear'])
+async def enable_auto_roll(ctx, category:str, rolls:int, minute:int):
+    global schedule
+    schedule = Scheduler()
+    schedule.hourly(dt.time(minute=minute, second=1), mudae.send_rolls, args=(category, rolls))
+    # schedule.hourly(dt.time(minute=minute, second=1), mudae.send_timer)
+    print(schedule)
+    await mudae_channel.send(f'Auto roll enabled')
+
+@is_me()
+@is_mudae_channel()
+@bot.command(name='checkar',
+             aliases=['car'])
+async def check_timers(ctx):
+    await mudae_channel.send(f'{schedule}')
 
 
+@is_me()
+@is_mudae_channel()
+@bot.command(name='checktimers',
+             aliases=['ct'])
+async def check_timers(ctx):
+    await mudae.send_timer()
 
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        pass
+    else:
+        print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
+        traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+bot.run(user_token)
 
-@client.event
-async def on_message(message):
-    # if message.author != client.user:
-    #     return
-
-    if message.content.startswith('$checktimers'):
-        await command_dict["Timer"].__call__(channel=channel_id)
-
-
-client.run(token)
 
--- mudae.py
+++ mudae.py
@@ -1,5 +1,17 @@
 import json
-import
+from time import sleep
 
-def checkTimer():
-    #for testing without spamming rolls going to use /tu first as proof of concept
+class MudaeCommander:
+    def __init__(self, command_dict, mudae_channel):
+        self.command_dict = command_dict
+        self.mudae_channel = mudae_channel
+
+    async def send_timer(self):
+        await self.command_dict["tu"].__call__(channel=self.mudae_channel.id)
+
+    async def send_rolls(self,category, amount:int):
+        for _ in range(amount):
+            await self.command_dict[f"{category}"].__call__(channel=self.mudae_channel.id)
+            # await self.mudae_channel.send(f'Dummy roll')
+            sleep(2)
+        return
